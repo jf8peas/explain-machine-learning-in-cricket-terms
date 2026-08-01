912f7fda17ec92d3e0a84c188294652c25016551
John Fong <john.fong@eightpeas.com>
2026-08-01 15:09:56 +1000

    add github deploy actions

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,5 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    site: 'https://jf8peas.github.io',
+    base: '/explain-machine-learning-in-cricket-terms/'
   },
 });
